e9eaee81e4c610e077b655b1e9f80701bae7f461
biaazinha <biancakaroliner04@gmail.com>
2023-02-21 17:28:42 -0300

    Array

---
+++ Curso 2 - arrays/adicionandoDado.js
@@ -0,0 +1,8 @@
+const nota = [10, 6, 8]
+//constante só não pode atribuir (=)
+//atribui o 7 ao array
+notas.push(7);
+
+const media = (notas[0] + notas[1] + notas[2]) + notas[3]/ notas.length;
+
+console.log(media);

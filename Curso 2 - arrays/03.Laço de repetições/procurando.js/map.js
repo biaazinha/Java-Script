85d802a7daf34306dc676c813c037a8e9ac8c5c6
biaazinha <biancakaroliner04@gmail.com>
2023-02-26 00:13:29 -0300

    Update map.js

--- Curso 2 - arrays/03.Laço de repetições/procurando.js/map.js
+++ Curso 2 - arrays/03.Laço de repetições/procurando.js/map.js
@@ -3,6 +3,6 @@ const notas = [10, 9.5, 8, 7, 6];
 const novasNotas = notas.map((nota) => {
     // if           se for? isso não?: continua
     return nota + 1 >= 10 ? 10 : nota + 1;
-})
+});
 
 console.log(novasNotas);

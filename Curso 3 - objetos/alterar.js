40e9681208d103087ae803766b1f181cdc38df52
Bianca Ramos <biancakaroliner04@gmail.com>
2023-02-28 16:46:26 -0300

    Curso 3

---
+++ Curso 3 - objetos/alterar.js
@@ -0,0 +1,9 @@
+const pessoa = {
+    nome: "Bia",                     //pode manipular uma const porém não pode atribuir nada (=)
+    profissao: "engenheira"
+};
+
+pessoa.telefone = "1122233344";   //adicionei
+pessoa.nome = "Luma"    //mudei
+
+console.log(pessoa);
---
+++ Curso 3 - objetos/objeto.js
@@ -0,0 +1,11 @@
+const listaCPF = [1111, 222222, 33333];
+
+const informacoes = ["nome", "José", "idade", "CPF", 32, "1122233356"];
+
+console.log(informacoes[1]);
+
+//objeto
+const objetoPessoa = {
+    nome: "José",
+    idade: 32,
+};
